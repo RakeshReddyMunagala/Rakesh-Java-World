125a5fe857bb9f9bc4db4f7500d9c875796dc05a
RakeshReddyMunagala <39795587+RakeshReddyMunagala@users.noreply.github.com>
2018-06-04 11:13:31 +0530

    Commit for reverse line and palidrome of String

---
+++ Palidrome_For_String.java
@@ -0,0 +1,42 @@
+﻿/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+     static	boolean ispal(String IntailString)
+     {
+	int length = IntailString.length();
+		int i = 0;
+		int j = length / 2;
+		while(i <= j){
+		if(IntailString.charAt(i) == IntailString.charAt(length-1-i))
+			{
+				i++;
+			}
+			else
+			{
+			return false;
+			}
+				
+		}
+	
+		return true;	
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	       if(ispal("NITVIN"))
+	       { 
+		System.out.println("Plindrome");
+	       }
+	       else
+	       {
+	       	System.out.println("NoT Plindrome");
+	       }
+		
+	}
+	
+}
---
+++ Reverse_line.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	static void findIndex(String orinalString){
+		
+		int length = orinalString.length();
+		String eachWord = "";
+		String finalString = "";
+		for(int i = 0; i < length ; i ++)
+		{
+			if (orinalString.charAt(i) == ' ')
+			{
+				finalString = " " + eachWord + finalString;
+				eachWord = "";
+			}else
+			{
+				 eachWord += Character.toString(orinalString.charAt(i));
+			}
+			
+		}
+		finalString = eachWord + finalString;
+		System.out.println(finalString);
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String orinalString = " My name Rakesh";
+		findIndex(orinalString);
+	}
+}
