a15a8c43dc187e61e69e2b6e98535c9a46f1177f
RakeshReddyMunagala <39795587+RakeshReddyMunagala@users.noreply.github.com>
2018-06-11 14:33:31 +0530

    Commiting

---
+++ Sum_of_number_divided_by_7_with_given_range.java
@@ -0,0 +1,37 @@
+﻿/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int finalNumber = 0;
+		
+		for( int i = 100 ; i <= 9000 ; i++)
+		{
+			if(i%7 == 0)
+			{
+			 finalNumber +=i;	
+			}
+		}
+		System.out.println(finalNumber);
+		
+		int sum =0;
+		int low = (100/7)+1;
+		int hi = 9000/7;
+		// System.out.println(low);
+		// System.out.println(hi);
+		
+		for(int i =low;i<=hi;i++){
+			sum+=i*7;
+		}
+		System.out.println(sum);
+		
+		
+		
+	}
+}
