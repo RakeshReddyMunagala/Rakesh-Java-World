3560e7ea3490f63bdcc8171ce4118ef58cc7fc43
RakeshReddyMunagala <39795587+RakeshReddyMunagala@users.noreply.github.com>
2018-06-11 16:49:01 +0530

    commit

---
+++ Factorail_number.java
@@ -0,0 +1,36 @@
+﻿/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	
+	
+	// Returns Factorial of N
+    static BigInteger factorial(int N)
+    {
+        // Initialize result
+        BigInteger f = new BigInteger("1"); // Or BigInteger.ONE
+ 
+        // Multiply f with 2, 3, ...N
+        for (int i = 2; i <= N; i++)
+            f = f.multiply(BigInteger.valueOf(i));
+ 
+        return f;
+    }
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		
+		int N = 15;
+	        System.out.println(factorial(N));
+
+		
+		
+	}
+}
